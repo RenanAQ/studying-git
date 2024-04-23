0bac44f5f40e07478543195f0a57216ebf60d9f7
RenanAQ <renanqueiroz96@gmail.com>
2024-04-23 08:54:17 -0400

    version2

--- list.c
+++ list.c
@@ -16,52 +16,6 @@ int main(void)
     *(list+1) = 2; //same as: list[1] = 2;
     *(list+2) = 3; //same as: list[2] = 3;
 
-    //In case I need more space in my array
-    int* tmp = malloc(4 * sizeof(int)); //we can't use int* list again because it would leak memory*/
-    
-    printf("Address: %p\n", &tmp);
-    if (tmp == NULL)
-    {
-        free(list); //Before breaking the code, i need to free the memory alloc of the first malloc
-        return 1;
-    }
-    
-     for (int i = 0; i < 3; i++)
-     {
-         tmp[i] = list[i]; //allocating the info to the tmp array
-     }
-    
-    printf("Address: %p\n", &tmp);
-    printf("Address: %p\n", &list);
-    tmp[3] = 4;
-    list = tmp; //pointing list to the new pointer array
-    
-    //free(list); //freeing the old list pointer
-
-    for (int i = 0; i < 4; i++)
-    {
-        printf("Value: %i ---> Addres: %p\n", list[i], &list[i]);
-    }
-    
-    free(list);
-    return 0;
-}
-
-/*
-Final version:
-    int* list = malloc(3 * sizeof(int)); //This is the same thing as: int list[3];
-
-    //checking if it is out of memory (Segmentation Fault)
-    if (list == NULL)
-    {
-        return 1;
-    }
-    
-
-    *list = 1; // This is the same as: list[0] = 1;
-    *(list+1) = 2; //same as: list[1] = 2;
-    *(list+2) = 3; //same as: list[2] = 3;
-
     //NOT NECESSARY BECAUSE WE'RE USING REALLOC
     //int* tmp = malloc(4 * sizeof(int)); //we can't use int* list again because it would leak memory
     
@@ -94,4 +48,4 @@ Final version:
     
     free(list);
     return 0;
-*/
+}
